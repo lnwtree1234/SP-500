5c3bf690081dc55afd1422a6e8470de57444f79e
Thdsws22 <64190144+lnwtree1234@users.noreply.github.com>
2023-07-05 18:02:41 +0700

    Update sp500-app.py

--- sp500-app.py
+++ sp500-app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import base64
 import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 import yfinance as yf
 
 st.title('S&P 500 App')
